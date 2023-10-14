9bc5aad339b94259663ca4f25e6a3ab77cdd138e
mkrishna <mkrishna@cure.com>
2023-10-15 02:27:07 +0530

    initialiaze plugin2

--- aceplugin.php
+++ aceplugin.php
@@ -1,4 +1,14 @@
 <?php
-/**
- * Plugin Name: AcePlugin
+/*
+ * Plugin Name:       Ace Plugin
+ * Description:       Just Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Author:            Sahoo
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       aceplugin
  */
+
+
+ ?>
